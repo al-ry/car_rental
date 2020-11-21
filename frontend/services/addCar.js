e30ff181cfb6650a8059cc5e995d037627bb5e6a
al-ry <saska_ryb@mail.ru>
2020-11-21 17:56:02 +0300

    Update addCar.js

--- frontend/services/addCar.js
+++ frontend/services/addCar.js
@@ -11,7 +11,7 @@
 import Axios from "axios";
 
 export async function addCar(data) {
-    
+
     const response = await fetch('/addCar', {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},

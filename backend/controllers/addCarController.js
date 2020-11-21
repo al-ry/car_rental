a4a84925de6ec53c22b0c29d7d82c5c01beed85f
al-ry <saska_ryb@mail.ru>
2020-11-21 17:43:11 +0300

    deleted

--- backend/controllers/addCarController.js
+++ backend/controllers/addCarController.js
@@ -3,8 +3,4 @@ exports.addCar = (req, res) => {
     const car = req.body
     console.log(car)
     res.sendStatus(200)
-<<<<<<< Updated upstream
 }
-=======
-} 
->>>>>>> Stashed changes
--- frontend/services/addCar.js
+++ frontend/services/addCar.js
@@ -1,13 +1,21 @@
 
+// export async function addCar(data) {
+//     const response = await fetch('/addCar', {
+//         method: 'POST',
+//         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+//         body: JSON.stringify(data)
+//     })
+//     return await response;
+// }
+
+import Axios from "axios";
+
 export async function addCar(data) {
+    
     const response = await fetch('/addCar', {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         body: JSON.stringify(data)
     })
     return await response;
-<<<<<<< Updated upstream
 }
-=======
-} 
->>>>>>> Stashed changes

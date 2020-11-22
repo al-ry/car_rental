a28bcc9d1b05784879948fd68004c42650bb16eb
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-11-22 15:16:40 +0300

    Added frontend side for user registration

--- frontend/src/components/Registration.vue
+++ frontend/src/components/Registration.vue
@@ -1,26 +1,23 @@
 <template>
     <el-form ref="form" :model="form" label-width="120px" class="registration-form">
         <span class="registration-label">Registration</span>
-        <el-form-item label="First name">
-            <el-input v-model="form.firstName"></el-input>
+        <el-form-item label="Name">
+            <el-input v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="Last name">
-            <el-input v-model="form.lastName"></el-input>
+        <el-form-item label="Phone">
+            <el-input v-model="form.phone"></el-input>
         </el-form-item>
-
-        <el-form-item label="Sity">
-            <el-select v-model="form.region" placeholder="please select your sity">
-                <el-option label="Казань" value="value1"></el-option>
-                <el-option label="Йошкар-Ола" value="value2"></el-option>
+        <el-form-item label="Email">
+            <el-input v-model="form.email" type="email"></el-input>
+        </el-form-item>
+        <el-form-item label="City">
+            <el-select v-model="form.city" placeholder="please select your city">
+                <el-option label="Казань" value="1"></el-option>
+                <el-option label="Йошкар-Ола" value="0"></el-option>
             </el-select>
         </el-form-item>
-
         <el-form-item label="Birthday">
             <el-input v-model="form.birthday" type="date" placeholder="Pick a day"></el-input>
-        </el-form-item>
-
-        <el-form-item label="Login">
-            <el-input v-model="form.login"></el-input>
         </el-form-item>
 
         <el-form-item label="Password">
@@ -28,9 +25,14 @@
         </el-form-item>
 
         <el-form-item label="Confirm password">
-            <el-input v-model="form.confirmPassrowd"></el-input>
+            <el-input v-model="form.confirmPassword"></el-input>
         </el-form-item>
-        
+        <el-form-item v-if="doesPasswordsMatch == false" class="error_message">
+            <span>Passwords does not math</span>
+        </el-form-item>
+        <el-form-item v-if="isEmptyInput == true" class="error_message">
+            <span>All fields should be filled</span>
+        </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="onSubmit">Create</el-button>
             <router-link class="go-back-button" to="/"><el-button>Go back to start page</el-button></router-link>
@@ -40,23 +42,53 @@
 
 
 <script>
+import {registerUser} from '../../services/registerUser'
   export default {
     data() {
       return {
         form: {
-            firstName: '',
-            lastName: '',
-            region: '',
+            name: '',
+            city: '',
+            email: '',
             password: '',
             confirmPassword: '',
             birthday: '',
-            login: ''
-        }
+            phone: ''
+        },
+
+        doesPasswordsMatch: true,
+        isEmptyInput: false,
       }
     },
     methods: {
       onSubmit() {
+        this.isEmptyInput = false
+        console.log("Form", this.form);
+        for(const field in this.form)
+        {
+            if(!this.form[field])
+            {
+                this.isEmptyInput = true;
+                console.log("error", field);
+                return;
+            }
+        } 
         
+        if (this.form.password != this.form.confirmPassword)
+        {
+            this.doesPasswordsMatch = false
+            return;
+        }
+
+        let user = {
+            name: this.form.name,
+            phone: this.form.phone,
+            email: this.form.email,
+            id_city: this.form.city,
+            password: this.form.password
+        }
+        console.log("test", user);
+        registerUser(user);
       },
 
       OnChange()
@@ -81,6 +113,11 @@
     display: block;
     font-size: 25px;
     max-width: 150px;
+}
+
+.error_message
+{
+    color : red;
 }
 
 </style>
